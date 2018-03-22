4b74b904569ab4d5f86a9542fb9296003d972280
schueler <schueler@c047opsi4.EINSTEIN>
2018-03-22 10:10:40 +0100

    Calculation calculateParabola() - Justin

--- src/kriegdernerds/Calculation.java
+++ src/kriegdernerds/Calculation.java
@@ -16,8 +16,9 @@ public class Calculation {
     private Map map;
     private Player activePlayer;
     
-    private int angle; //Attribut für den Schusswinkel
-   // private int velocity = projectile //Attribut wichtig für Streckfaktor, leicht runtergeregelt
+    private double velocity = projectile.getVelocity();
+    private double angle;
+    private double height = player.getPositionY();
     
     /**
      * Konstruktor
@@ -43,10 +44,6 @@ public class Calculation {
     }
     
     public void calculatePlayerDamage(Player player, Projectile projectile) {
-        //Variablen für Funktion der Parabel
-        double a = Math.tan(angle); //Streckfaktor
-        
-        double x; //Abhängige Variable für Funktion
     }
     
     /**
@@ -54,7 +51,46 @@ public class Calculation {
      * wenn ein Projektil abgeschossen wird
      */
     public void calculateParabola(Projectile projectile) {
+                
+        /*
+        Die Variable a ist der Faktor mit der die Parabel beim Schiessen des
+        Projektils gestreckt bzw. gestaucht wird. Weil diese nach unten
+        geoeffnet sein muss, ist dieser Wert negativ. Damit die Parabel nicht zu
+        stark beeinflusst wird, wird diese um dem Quotienten 10 verkleinert.
         
+        Die Variable b ist für die Steigung des Projektils am Ort des Spielers
+        verantwortlich. Wenn der Schusswinkel angle gegeben ist, so bekommt man
+        die Steigung, indem man den Schusswinkel in den Tangens einsetzt. Da die
+        Methode Math.tan() mit Werten im Bogenmaß rechnet und auch diese ausgibt,
+        muss man den Schusswinkel erst in das Bogenmaß umrechnen, was mit der
+        Formel "Winkel geteilt durch 180, und das multipliziert mit Pi" möglich
+        ist. Das Ergebnis im Tangens eingesetzt wird dann noch einmal auf zwei
+        Stellen gerundet.
+        
+        Die Variable c entspricht der Variablen height. Sie wird hier doppelt
+        initialisiert, damit der Code darunter etwas geordneter aussieht
+        */
+        double a = -velocity / 10;
+        double b = Math.round(Math.tan(angle/180.0*Math.PI)*100.0)/100.0;
+        double c = height;
+        System.out.println(angle +" "+ b);
+        
+        /*
+        Die Variable impactPoint1 berechnet die Nullstelle der Quadratischen
+        Funktion mithilfe der abc-Formel.
+        Da eine quadratische Funktion auch zwei Nullstellen haben kann, muessen
+        zwei moegliche Nullstellen berechnet werden. Dies geht, indem man das
+        Vorzeichen der Wurzel einmal positiv hat und einmal negativ.
+        Die Lösung mit dem negativen Vorzeichen vor der Wurzel ist die Variable
+        impactPoint2.
+        */
+        double impactPoint1 = Math.round(((-b + Math.sqrt(b*b - 4*a*c)) / (2*a))*100.0)/100.0;
+        double impactPoint2 = Math.round(((-b - Math.sqrt(b*b - 4*a*c)) / (2*a))*100.0)/100.0;
+        
+        //Die Schleife berechnet alle Punkte der Parabel bis sie am Boden ankommt
+        for(double x=0; x <= impactPoint2; x = x+0.1){
+            double parabola = a*x*x + b*x + c;
+        }
     }
     
     private void calculateMapDamage(Map map, Projectile projectile) {
